b760f7b81fe9dcb6fe1e44544cd0fbcf209e9e78
JulianLopez2709 <judalomor27.95@gmail.com>
2025-04-16 13:09:41 -0500

    v1

--- src/templates/AdminLayout.tsx
+++ src/templates/AdminLayout.tsx
@@ -1,55 +1,78 @@
 import { Button } from "@/components/ui/button"
 import { House, LogOut, ScrollText, Settings } from "lucide-react"
-import { Link, Outlet } from "react-router-dom"
-
+import { Link, Outlet, useLocation } from "react-router-dom"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
 
 function AdminLayout() {
-    return (
-        <>
-            <header className="border-b-2 border-gray-100 p-5 flex justify-between items-center">
-                <div className="flex gap-5">
-                    <div className="w-10 h-5">LOGO</div>
-                    <p>name section</p>
-                </div>
-                <div>perfil avatar</div>
-            </header>
-            <div className="h-screen flex">
-                <nav className="h-full flex-col justify-between border-r-2 border-gray-100 ">
-                    <div className="p-2  flex flex-col ">
-                        
-                        <Button  className="bg-[#2CEE12] size-[60px] rounded-2xl opacity-50">
-                            <Link to="/admin" className="flex items-center gap-2 py-7">
-                                <House />
-                            </Link>
-                        </Button>
-                        <Button  className="bg-[#12EE96] size-[60px] rounded-2xl opacity-50">
-                            <Link to="/admin/products" className="flex items-center gap-2 py-7">
-                                <p className="font-bold h-full w-full">P</p>
-                            </Link>
-                        </Button>
-                        <Button  className="bg-[#123EEE] size-[60px] rounded-2xl opacity-50">
-                            <Link to="/admin/orders" className="flex items-center gap-2 py-7">
-                                <ScrollText />
-                            </Link>
-                        </Button>
-                        <Button className="bg-[#B012EE] size-[60px] rounded-2xl opacity-50">
-                            <Link to="/admin/settings" className="flex items-center gap-2 py-7">
-                                <Settings />
-                            </Link>
-                        </Button>
+    const location = useLocation()
 
+    const getSectionTitle = () => {
+        switch(location.pathname) {
+            case '/admin':
+                return 'Inicio'
+            case '/admin/products':
+                return 'Productos'
+            case '/admin/orders':
+                return 'Pedidos'
+            case '/admin/settings':
+                return 'Configuración'
+            default:
+                return 'Section'
+        }
+    }
+
+    const isActive = (path: string) => {
+        return location.pathname === path
+    }
+
+
+    return (
+        <div className="h-screen flex flex-col">
+            <header className="border-b-2 border-gray-100 p-2 md:p-5 flex justify-between items-center">
+                <div className="flex text-2xl gap-5 ">
+                    <div className="w-10 h-5 text-green-700 font-bold mr-2">AMIR</div>
+                    <p className="font-bold">{getSectionTitle()}</p>
+                </div>
+                <Avatar>
+                    <AvatarImage src="https://github.com/shadcn.png" />
+                    <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+            </header>
+            <div className="flex-1 flex overflow-hidden">
+                <nav className="flex flex-col justify-between border-r-2 border-gray-100 ">
+                    <div className="p-1 md:p-2 flex flex-col gap-y-3">
+                    <Link to="/admin" className="size-[60px]">
+                            <Button className={`cursor-pointer w-full h-full rounded-2xl flex items-center justify-center ${isActive('/admin') ? 'bg-[#2CEE12] opacity-100' : 'bg-[#2CEE12] opacity-50'}`}>
+                                <House />
+                            </Button>
+                        </Link>
+                        <Link to="/admin/products" className="size-[60px]">
+                            <Button className={`cursor-pointer w-full h-full rounded-2xl flex items-center justify-center ${isActive('/admin/products') ? 'bg-[#12EE96] opacity-100' : 'bg-[#12EE96] opacity-50'}`}>
+                                <p className="font-bold m-auto">P</p>
+                            </Button>
+                        </Link>
+                        <Link to="/admin/orders" className="size-[60px]">
+                            <Button className={`cursor-pointer w-full h-full rounded-2xl flex items-center justify-center ${isActive('/admin/orders') ? 'bg-[#123EEE] opacity-100' : 'bg-[#123EEE] opacity-50'}`}>
+                                <ScrollText />
+                            </Button>
+                        </Link>
+                        <Link to="/admin/settings" className="size-[60px]">
+                            <Button className={`cursor-pointer w-full h-full rounded-2xl flex items-center justify-center ${isActive('/admin/settings') ? 'bg-[#B012EE] opacity-100' : 'bg-[#B012EE] opacity-50'}`}>
+                                <Settings />
+                            </Button>
+                        </Link>
                     </div>
-                    <Button className="bg-green-700 hover:bg-green-800 text-white rounded-none">
-                        <Link to="/" className="flex items-center gap-2 py-7">
+                    <Link to="/" className="w-full">
+                        <Button className=" cursor-pointer w-full h-full items-center bg-green-700 hover:bg-green-800 text-white rounded-none py-7">
                             <LogOut />
                             <span>Salir</span>
-                        </Link>
-                    </Button>
+                        </Button>
+                    </Link>
                 </nav>
-                <main className="bg-[#EFEFEF] h-full w-full"><Outlet /></main>
+                <main className="bg-[#EFEFEF] flex-1  w-full"><Outlet /></main>
             </div>
-        </>
+        </div>
     )
 }
 
