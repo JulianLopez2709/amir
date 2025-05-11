8e5b231d4d8025fdf8f739133297f8d9ed8510d6
JulianLopez2709 <judalomor27.95@gmail.com>
2025-05-11 08:35:12 -0500

    color dinamic

--- src/templates/AdminLayout.tsx
+++ src/templates/AdminLayout.tsx
@@ -2,6 +2,9 @@ import { Button } from "@/components/ui/button"
 import { House, LogOut, ScrollText, Settings } from "lucide-react"
 import { Link, Outlet, useLocation } from "react-router-dom"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { useEffect } from "react"
+import Company from "@/@types/Company"
+
 
 
 function AdminLayout() {
@@ -15,7 +18,7 @@ function AdminLayout() {
                 return 'Productos'
             case '/admin/orders':
                 return 'Pedidos'
-            case '/admin/settings':
+            case '/admin/setting':
                 return 'Configuración'
             default:
                 return 'Section'
@@ -25,6 +28,23 @@ function AdminLayout() {
     const isActive = (path: string) => {
         return location.pathname === path
     }
+    
+    const company: Company = {
+        id: 1,
+        type: "Restaurante",
+        name: "Mi Restaurante",
+        slogan: "La mejor comida del mundo",
+        logo: "https://example.com/logo.png",
+        primary_color: "#000088",
+        secundary_color: "#C70039",
+        plan: "Premium"
+    }
+
+    useEffect(() => {
+        // Establecer colores como variables CSS
+        document.documentElement.style.setProperty("--primary-color", company.primary_color);
+        document.documentElement.style.setProperty("--secondary-color", company.secundary_color);
+    }, [company]);
 
 
     return (
@@ -43,29 +63,29 @@ function AdminLayout() {
                 <nav className="flex flex-col justify-between border-r-2 border-gray-100 ">
                     <div className="p-1 md:p-2 flex flex-col gap-y-3">
                         <Link to="/admin" className="size-[60px]">
-                            <Button className={`cursor-pointer w-full h-full rounded-2xl flex items-center justify-center ${isActive('/admin') ? 'bg-[#2CEE12] opacity-100' : 'bg-[#2CEE12] opacity-50'}`}>
+                            <Button className={`cursor-pointer w-full h-full rounded-2xl flex items-center justify-center ${isActive('/admin') ? 'bg-[#2CEE12] opacity-100 border-2 border-gray-200' : 'bg-[#2CEE12] opacity-50'}`}>
                                 <House />
                             </Button>
                         </Link>
                         <Link to="/admin/products" className="size-[60px]">
-                            <Button className={`cursor-pointer w-full h-full rounded-2xl flex items-center justify-center ${isActive('/admin/products') ? 'bg-[#12EE96] opacity-100' : 'bg-[#12EE96] opacity-50'}`}>
+                            <Button className={`cursor-pointer w-full h-full rounded-2xl flex items-center justify-center ${isActive('/admin/products') ? 'bg-[#12EE96] opacity-100 border-2 border-gray-200' : 'bg-[#12EE96] opacity-50'}`}>
                                 <p className="font-bold m-auto">P</p>
                             </Button>
                         </Link>
                         <Link to="/admin/orders" className="size-[60px]">
-                            <Button className={`cursor-pointer w-full h-full rounded-2xl flex items-center justify-center ${isActive('/admin/orders') ? 'bg-[#123EEE] opacity-100' : 'bg-[#123EEE] opacity-50'}`}>
+                            <Button className={`cursor-pointer w-full h-full rounded-2xl flex items-center justify-center ${isActive('/admin/orders') ? 'bg-[#123EEE] opacity-100 border-2 border-gray-200' : 'bg-[#123EEE] opacity-50'}`}>
                                 <ScrollText />
                             </Button>
                         </Link>
                         <Link to="/admin/setting" className="size-[60px]">
-                            <Button className={`cursor-pointer w-full h-full rounded-2xl flex items-center justify-center ${isActive('/admin/settings') ? 'bg-purple-700 opacity-100' : 'bg-[#B012EE] opacity-50'}`}>
+                            <Button className={`cursor-pointer w-full h-full rounded-2xl flex items-center justify-center ${isActive('/admin/setting') ? 'bg-purple-700 opacity-100 border-2 border-gray-200' : 'bg-[#B012EE] opacity-50'}`}>
                                 <Settings />
                             </Button>
                         </Link>
                     </div>
                     <Link to="/" className="w-full">
-                        <Button className=" cursor-pointer w-full h-full items-center bg-green-700 hover:bg-green-800 text-white rounded-none py-7">
-                            <LogOut />
+                        <Button className=" cursor-pointer w-full h-full items-center hover:bg-green-800 text-white rounded-none py-7 font-bold" style={{background : 'var(--primary-color)'}}>
+                            <LogOut className=""/>
                             <span>Salir</span>
                         </Button>
                     </Link>
