4d296a39bbf689c3d730af6f04fc09fd5da86c8f
JulianLopez2709 <judalomor27.95@gmail.com>
2025-06-01 16:36:16 -0500

    refactor LoginPage component for improved user experience

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -4,6 +4,7 @@ import { Link, useNavigate } from "react-router-dom"
 import inventariadoImg from '../../assets/inventariadoImg.webp'
 import { useState } from "react"
 import { login } from "@/api/auth/login"
+import Cookies from "js-cookie"
 
 function LoginPage() {
   const [email, setEmail] = useState("")
@@ -11,7 +12,6 @@ function LoginPage() {
   const navigate = useNavigate();
 
   const handleLogin = async () => {
-    console.log("Iniciando sesión con:", email, password)
     if (!email || !password) {
       return
     }
@@ -19,9 +19,10 @@ function LoginPage() {
     try {
       const responde = await login(email, password)
       if (responde) {
-        console.log("Inicio de sesión exitoso:", responde)
-        localStorage.setItem("token", responde.token)
+        const cook = Cookies.get("token") 
         localStorage.setItem("user", JSON.stringify(responde))
+        //localStorage.setItem("primary_color", JSON.stringify(responde.companies[0].primary_color))
+        //localStorage.setItem("secondary_color", JSON.stringify(responde.companies[0].secundary_color))
         navigate("/admin/products")
       } else {
         alert("Error al iniciar sesión. Por favor, verifica tus credenciales.")
@@ -29,34 +30,32 @@ function LoginPage() {
     } catch (error) {
       console.error("Error al iniciar sesión:", error);
       alert("Error al iniciar sesión. Por favor, verifica tus credenciales.");
-      setEmail("");
-      setPassword("");
     }
-
   }
 
   return (
     <div className="h-screen md:flex">
       <div className="w-96 h-full m-auto flex flex-col items-center justify-center">
-        <h2 className="font-extrabold py-5 text-8xl text-green-700 ">Amin</h2>
-        <h1 className="font-bold text-3xl">Ingresa a tu cuenta</h1>
-        <h2 className="pb-10 text-xl">Gana tiempo y administra tu empresa.</h2>
-        <div className="w-full py-3">
-          <label htmlFor="email">Nombre de usuario o correo electronico</label>
-          <Input className="py-5 text-xl" type="text" id="email" placeholder="Ingrese email o username" value={email} onChange={(e) => setEmail(e.target.value)} />
-        </div>
-        <div className="w-full py-3">
-          <div className="flex justify-between">
-            <label htmlFor="password">Password</label>
-            <p className="text-sm text-green-700 opacity-70">¿Olvidaste tu contraseña?</p>
+
+          <h2 className="font-extrabold py-5 text-8xl text-green-700 ">Amin</h2>
+          <h1 className="font-bold text-3xl">Ingresa a tu cuenta</h1>
+          <h2 className="pb-10 text-xl">Gana tiempo y administra tu empresa.</h2>
+          <div className="w-full py-3">
+            <label htmlFor="email">Nombre de usuario o correo electronico</label>
+            <Input className="py-5 " type="text" id="email" placeholder="Ingrese email o username" value={email} onChange={(e) => setEmail(e.target.value)} required />
           </div>
-          <Input className="py-5" type="password" id="password" placeholder="Ingrese Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        </div>
-        <Button className="bg-green-700 w-full p-5" onClick={handleLogin}>Ingresar</Button>
-        <p className="text-sm opacity-70">Ingresa como asesor o administrador</p>
-        <Link to="/">
-          <p className="opacity-70 text-green-700">¿Necesitas ayuda?</p>
-        </Link>
+          <div className="w-full py-3">
+            <div className="flex justify-between">
+              <label htmlFor="password">Password</label>
+              <p className="text-sm text-green-700 opacity-70">¿Olvidaste tu contraseña?</p>
+            </div>
+            <Input className="py-5" type="password" id="password" placeholder="Ingrese Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+          </div>
+          <Button className="bg-green-700 w-full p-5" onClick={handleLogin}>Ingresar</Button>
+          <p className="text-sm opacity-70">Ingresa como asesor o administrador</p>
+          <Link to="/">
+            <p className="opacity-70 text-green-700">¿Necesitas ayuda?</p>
+          </Link>
       </div>
       <div className="h-full md:w-[50%] flex flex-col justify-between items-center rounded-tl-4xl relative overflow-hidden">
         <div className="absolute inset-0 bg-gradient-to-t from-80% from-black to-transparent ">
